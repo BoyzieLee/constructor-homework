b29d2e7543dc942e67c634e443e20e49faecab7c
Boyzie Mathis <boyzielee@gmail.com>
2016-05-04 08:59:33 -0400

    working on last 3.

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -4,5 +4,9 @@ let Dog = function (params) {
 
   this.status = 'normal';
   this.color = localParams.color;
+  // set a default of undefined, !== means not equal to, || means or,  ? : is conditional
+  this.hungry = (localParams.hungry !== undefined) ? localParams.hungry : true;
+  this.feed = 'true';
+
 }
 export default Dog;
--- src/js/constructors/human.js
+++ src/js/constructors/human.js
@@ -1,6 +1,19 @@
-let Human = function () {
+let Human = function (passedOptions) {
+
+  let options = passedOptions || {};
+
   this.pet = function (petObj) {
     petObj.status = 'happy';
+    // petObj.hungry = options.hungry;
   };
+  this.feed = function (Dog) {
+    return Dog.hungry = 'false'
+    // set a default of undefined, !== means not equal to, || means or,  ? : is conditional
+    // this.cool = (passedOptions.cool !== undefined) ? passedOptions.cool : true;
+    this.cool = passedOptions.cool || true;
+  };
+
+
 }
+
 export default Human;
